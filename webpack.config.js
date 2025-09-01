18c2ea1837a52dc9d57196392802b0a64cffc5e0
Gustavo dias <dscharraa@gmail.com>
2025-09-01 18:59:33 -0300

    fix: tirando node_modules do rastreamento

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // seu template base
+      filename: 'index.html',
+    })
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    port: 9000,
+    open: true,
+  },
+  mode: 'production'
+};
